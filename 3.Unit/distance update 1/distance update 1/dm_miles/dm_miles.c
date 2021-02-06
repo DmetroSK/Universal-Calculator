1cd21e6ff3110fce54147490c52fc1391be6862a
DmetroSK <ygpramuditha@gmail.com>
2021-02-06 23:42:32 +0530

    Final Release

--- 3.Unit/distance update 1/distance update 1/dm_miles/dm_miles.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{float m, mi;
-    printf("Enter the distance in meters : ");
-    scanf("%f",&m);
-         mi = m /1609.34;
-    printf("%.2f m = %.9f miles\n\n",m,mi);
-return 0;
-}
